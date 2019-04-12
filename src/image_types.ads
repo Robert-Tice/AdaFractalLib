c85aae6b3e222aa202049beec9bc3ffdb1a6107e
Robert Tice <tice@adacore.com>
2019-04-12 10:48:45 -0400

    Initial code commit. Removing fractal section of code from AdaFractal project and moving it to its own lib for reuse.

---
+++ src/image_types.ads
@@ -0,0 +1,53 @@
+package Image_Types is
+
+   subtype ImgWidth is Positive range 1 .. 1920;
+   subtype ImgHeight is Positive range 1 .. 1080;
+   subtype ImgZoom is Positive range 1 .. 1000;
+
+   type Coordinate is record
+      X : ImgWidth := ImgWidth'First;
+      Y : ImgHeight := ImgHeight'First;
+   end record;
+
+   type Viewport_Info is record
+      Width  : ImgWidth := ImgWidth'Last;
+      Height : ImgHeight := ImgHeight'Last;
+      Zoom   : ImgZoom := 10;
+      Center : Coordinate;
+   end record;
+
+   Pixel_Size : constant := 4;  --  bytes
+
+   type Color is new Natural range 0 .. 255
+     with Size => 8;
+
+   Max_Iterations : constant := Color'Last / 5;
+
+   type Pixel is record
+      Red   : Color;
+      Green : Color;
+      Blue  : Color;
+      Alpha : Color;
+   end record
+     with Size => 32;
+
+   for Pixel use record
+      Red at 0 range 0 .. 7;
+      Green at 1 range 0 .. 7;
+      Blue at 2 range 0 .. 7;
+      Alpha at 3 range 0 .. 7;
+   end record;
+
+   type Pixel_Array is array
+     (Natural range <>) of Pixel
+     with Pack;
+
+   type Pixel_Array_Ptr is access all Pixel_Array;
+
+   type Buffer_Element is mod 2 ** Standard'Storage_Unit;
+   type Buffer_Offset is new Long_Long_Integer;
+   type Buffer_Array is array (Buffer_Offset range <>) of Buffer_Element;
+   type Buffer_Access is access Buffer_Array;
+
+
+end Image_Types;
